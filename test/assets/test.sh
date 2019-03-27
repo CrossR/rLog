bff3fa28aa364a55b0fe44cd8a78af440c9617f2
CrossR <r.cross@lancaster.ac.uk>
2019-03-27 23:11:39 +0000

    Add a test script.

---
+++ test/assets/test.sh
@@ -0,0 +1,50 @@
+echo "Line 1"
+echo "Line 2" >&2
+echo "Line 3"
+echo "Line 4" >&2
+echo "Line 5"
+echo "Line 6" >&2
+echo "Line 7"
+echo "Line 8" >&2
+echo "Line 9"
+echo "Line 10" >&2
+echo "Line 11"
+echo "Line 12" >&2
+echo "Line 13"
+echo "Line 14" >&2
+echo "Line 15"
+echo "Line 16" >&2
+echo "Line 17"
+echo "Line 18" >&2
+echo "Line 19"
+echo "Line 20" >&2
+echo "Line 21"
+echo "Line 22" >&2
+echo "Line 23"
+echo "Line 24" >&2
+echo "Line 25"
+echo "Line 26" >&2
+echo "Line 27"
+echo "Line 28" >&2
+echo "Line 29"
+echo "Line 30" >&2
+echo "Line 31"
+echo "Line 32" >&2
+echo "Line 33"
+echo "Line 34" >&2
+echo "Line 35"
+echo "Line 36" >&2
+echo "Line 37"
+echo "Line 38" >&2
+echo "Line 39"
+echo "Line 40" >&2
+echo "Line 41"
+echo "Line 42" >&2
+echo "Line 43"
+echo "Line 44" >&2
+echo "Line 45"
+echo "Line 46" >&2
+echo "Line 47"
+echo "Line 48" >&2
+echo "Line 49"
+echo "Line 50" >&2
